dcac7e8faf572ef1cdab2cbdf83e1daad458a7a7
loki666 <simons.philippe@gmail.com>
2011-08-24 10:11:12 +0000

    android: build Box2D as a shared lib

---
+++ Box2D_2.1.3/build/android/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := Box2D
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).so
+LOCAL_STATIC_LIBRARIES := libstdc++
+include $(PREBUILT_SHARED_LIBRARY)
