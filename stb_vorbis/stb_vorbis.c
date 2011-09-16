f2e205c12a30aa1263f2db216c3af3ab7e577df3
iarwain <iarwain@orx-project.org>
2011-09-16 08:59:38 +0000

    - Renamed Point structure in stb_vorbis.c to avoid a conflict on iOS

--- stb_vorbis/stb_vorbis.c
+++ stb_vorbis/stb_vorbis.c
@@ -1223,12 +1223,12 @@ static void neighbors(uint16 *x, int n, int *plow, int *phigh)
 typedef struct
 {
    uint16 x,y;
-} Point;
+} stPoint;
 
 int point_compare(const void *p, const void *q)
 {
-   Point *a = (Point *) p;
-   Point *b = (Point *) q;
+   stPoint *a = (stPoint *) p;
+   stPoint *b = (stPoint *) q;
    return a->x < b->x ? -1 : a->x > b->x;
 }
 
@@ -3871,7 +3871,7 @@ static int start_decoder(vorb *f)
             g->book_list[j] = get_bits(f,8);
          return error(f, VORBIS_feature_not_supported);
       } else {
-         Point p[31*8+2];
+         stPoint p[31*8+2];
          Floor1 *g = &f->floor_config[i].floor1;
          int max_class = -1; 
          g->partitions = get_bits(f, 5);
