26f6b675e94eb70a70b776d5404eaa5177427a41
loki666 <simons.philippe@gmail.com>
2013-11-04 12:08:53 +0100

    android: use Tremolo for vorbis decoding on ARM

--- Tremolo/build/android/jni/Android.mk
+++ Tremolo/build/android/jni/Android.mk
@@ -2,24 +2,30 @@ LOCAL_PATH := $(call my-dir)/../../..
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE = Tremor
+LOCAL_MODULE = Tremolo
 LOCAL_SRC_FILES = \
 	bitwise.c      \
+        bitwiseARM.S   \
         codebook.c     \
+        dpen.S         \
         dsp.c          \
         floor0.c       \
         floor1.c       \
+        floor1LARM.S   \
         floor_lookup.c \
         framing.c      \
         info.c         \
         mapping0.c     \
         mdct.c         \
+        mdctLARM.S     \
         misc.c         \
         res012.c       \
-        vorbisfile.c
+        vorbisfile.c   \
+        speed.S
 
-LOCAL_CFLAGS := -DANDROID -DUSE_MEMORY_H -DHAVE_ALLOCA_H -D_LOW_ACCURACY_
+LOCAL_CFLAGS := -DANDROID -DUSE_MEMORY_H -DHAVE_ALLOCA_H -D_LOW_ACCURACY_ -D_ARM_ASSEM_
 
+LOCAL_ARM_MODE := arm
 TARGET_PLATFORM = android-9
 
 include $(BUILD_STATIC_LIBRARY)
