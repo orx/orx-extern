1bb2fd28844327f67f98a2eb977e1accf0eaa61d
loki666 <simons.philippe@gmail.com>
2013-11-22 11:33:47 +0100

    - android: recompile extern libs with gcc 4.8

--- Box2D_2.1.3/build/android/jni/Application.mk
+++ Box2D_2.1.3/build/android/jni/Application.mk
@@ -1,4 +1,3 @@
 APP_ABI := armeabi armeabi-v7a x86
-
 APP_MODULES := Box2D
-
+NDK_TOOLCHAIN_VERSION := 4.8
--- Tremolo/build/android/jni/Application.mk
+++ Tremolo/build/android/jni/Application.mk
@@ -1,4 +1,3 @@
 APP_ABI := armeabi armeabi-v7a x86
-
 APP_MODULES := Tremolo
-
+NDK_TOOLCHAIN_VERSION := 4.8
