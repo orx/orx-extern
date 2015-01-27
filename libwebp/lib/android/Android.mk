b0e74d32327a3975b5ab8ef8c70d6d78a478a8ae
loki666 <simons.philippe@gmail.com>
2015-01-27 14:29:15 +0100

    android: replace libwebp.a with libwebpdecoder.a
    --HG--
    branch : webp

--- libwebp/lib/android/Android.mk
+++ libwebp/lib/android/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := WebP-prebuilt
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebp.a
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebpdecoder.a
 TARGET_PLATFORM = android-9
 
 LOCAL_STATIC_LIBRARIES += cpufeatures
