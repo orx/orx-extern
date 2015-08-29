986cb42cf34f70a1c24bbccd0d4ce10a68886d7d
loki666 <simons.philippe@gmail.com>
2015-08-30 00:48:00 +0200

    android: revert to armeabi-v7a (no hard)
    --HG--
    branch : liquidfun

--- libwebp/lib/android/Android.mk
+++ libwebp/lib/android/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := WebP-prebuilt
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebpdecoder.a
-TARGET_PLATFORM = android-9
+TARGET_PLATFORM = android-10
 
 LOCAL_STATIC_LIBRARIES += cpufeatures
 
