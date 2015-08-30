b39cf53fe9ad849d97949b4636e397ad0d1b9505
loki666 <simons.philippe@gmail.com>
2015-08-30 08:15:39 +0200

    backout previous commit
    --HG--
    branch : liquidfun

--- Tremolo/lib/android/Android.mk
+++ Tremolo/lib/android/Android.mk
@@ -4,5 +4,5 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := Tremolo-prebuilt
 LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libTremolo.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../..
-TARGET_PLATFORM = android-10
+TARGET_PLATFORM = android-9
 include $(PREBUILT_STATIC_LIBRARY)
