b228d4ddadb10497d6d053c3c0bdd18cb4cbb67e
iarwain <iarwain@orx-project.org>
2019-10-10 00:58:41 -0400

    - Updated premake4 with better support for multiple targets in codelite projects - Updated premake4 windows binaries

--- premake/src/actions/codelite/codelite_project.lua
+++ premake/src/actions/codelite/codelite_project.lua
@@ -66,8 +66,8 @@
 		for _, platform in ipairs(platforms) do
 			for cfg in premake.eachconfig(prj, platform) do
 				local name = premake.esc(cfg.longname):gsub("|","_")
-				local compiler = iif(platform == "x64", "mingw64", "mingw32")
-				_p('    <Configuration Name="%s" CompilerType="MinGW ( %s )" DebuggerType="GNU gdb debugger" Type="%s">', name, compiler, types[cfg.kind])
+				local compiler = iif(os.is("windows"), iif(platform == "x64", "MinGW ( mingw64 )", "MinGW ( mingw32 )"), "GCC")
+				_p('    <Configuration Name="%s" CompilerType="%s" DebuggerType="GNU gdb debugger" Type="%s">', name, compiler, types[cfg.kind])
 
 				local fname  = premake.esc(cfg.buildtarget.fullpath)
 				local objdir = premake.esc(cfg.objectsdir)
