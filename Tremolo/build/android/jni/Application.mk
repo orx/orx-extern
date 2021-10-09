d18062f29479c8e84894787c81995f407833efa6
iarwain <iarwain@orx-project.org>
2021-10-08 22:21:03 -0400

    - Removed Tremolo from dependencies

--- Tremolo/build/android/jni/Application.mk
+++
@@ -1,3 +0,0 @@
-APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
-APP_MODULES := Tremolo
-NDK_TOOLCHAIN_VERSION := clang
--- Tremolo/lib/android/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := Tremolo-prebuilt
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libTremolo.a
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../..
-TARGET_PLATFORM = android-19
-include $(PREBUILT_STATIC_LIBRARY)
