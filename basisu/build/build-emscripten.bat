7cf85d4a4dbabbec74b19e5da6bf9ed696898580
iarwain <iarwain@orx-project.org>
2025-02-25 18:41:03 -0500

    - Removed BasisU C wrapper - Fixed a couple of issues in BasisU to be used outside the wrapper

--- basisu/build/build-emscripten.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cmd /c "emcc -c ../src/basisu.cpp -I../include -pthread -o basisu.o"
-cmd /c "emcc -c ../include/basisu/basisu_transcoder.cpp -I../include -pthread -o basisu_transcoder.o"
-mkdir ..\lib\web
-emar -rcs ../lib/web/libbasisu.a basisu.o basisu_transcoder.o
