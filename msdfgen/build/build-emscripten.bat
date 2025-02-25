7c5fb94367412dabc2b8a77953fba4f84ea9cd2b
iarwain <iarwain@orx-project.org>
2025-02-24 22:56:43 -0500

    - Removed MSDFGen wrapper and all the prebuilt binaries (using all-in-one C++ msdfgen directly)

--- msdfgen/build/build-emscripten.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-cmd /c "emcc -DMSDFGEN_NO_FREETYPE -c ../src/msdfgen/msdfgen.cpp -I../include -o msdfgen.o"
-cmd /c "emcc -DMSDFGEN_NO_FREETYPE -c ../src/msdfgen_c.cpp -I../include -o msdfgen_c.o"
-mkdir ..\lib\web
-emar -rcs ../lib/web/libmsdfgen.a msdfgen.o msdfgen_c.o
